7c3a0813db3024cd6555dcf74cbf5714121d0f56
Matt Proud <mproud@mattproud.com>
2026-09-24 12:41:07 -0400

    Updates

--- lib/site.ts
+++ lib/site.ts
@@ -6,4 +6,4 @@ export const SITE_DESCRIPTION =
 export const SITE_OG_TITLE = 'Sign the Petition - Support Cocktails to Go'
 export const SITE_OG_DESCRIPTION =
     'Sign the petition to bring cocktails to go to Anne Arundel County!'
-export const SITE_OG_IMAGE = '/images/aa-to-go-og-share-1200x630.jpg'
+export const SITE_OG_IMAGE = '/images/anne-arundel-to-go-og-share-1200x630.jpg'
